bc47da6aceae8e88c0b63925f110d4c8ae910929
Luan Belem Rivello <luanrivello@hotmail.com>
2025-12-05 15:41:36 -0300

    Day03 part01

--- src/main/kotlin/day03/Main.kt
+++ src/main/kotlin/day03/Main.kt
@@ -8,17 +8,44 @@ var answer:Long = 0
 
 fun main() {
     val time = measureNanoTime {
-        val input  = readFile("/day03/example.txt")
+        val inputFile  = readFile("/day03/input.txt")
 
+        inputFile.forEachLine { line ->
+            findMaximumVoltage(line)
+        }
     }
 
     println("Execution time: ${time / 1_000_000_000.0}s")
     println("Answer: $answer")
 }
 
-private fun readFile(path: String): String {
+private fun readFile(path: String): File {
     val inputURL = {}.javaClass.getResource(path)
-    val inputFile = File(inputURL.toURI())
+    return File(inputURL.toURI())
+}
 
-    return inputFile.readText().trim()
+fun findMaximumVoltage(line: String) {
+    val digits = line.map { it.digitToInt() }
+
+    var biggestValue = 0
+    var biggestIndice = 0
+    for (i in 0..digits.size-2) {
+        if (digits[i] > biggestValue) {
+            biggestValue = digits[i]
+            biggestIndice = i
+        }
+    }
+
+    var complementValue = 0
+    for (i in biggestIndice+1 until digits.size) {
+        if (digits[i] > complementValue) {
+            complementValue = digits[i]
+        }
+    }
+
+    answer += biggestValue*10 + complementValue
+
+    //println("Digits: $digits")
+    println("Voltage: $biggestValue$complementValue")
+    //println("Answer: $answer")
 }
